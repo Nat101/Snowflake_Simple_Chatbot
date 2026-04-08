afa83cfbaa28cf2e1fb5a4a4880c73591d2ee6d8
Natalie Carlson <nataliecarlson78@gmail.com>
2026-04-07 22:13:07 -0800

    Update 02_Create_Stage_LoadData.sql

--- 02_Create_Stage_LoadData.sql
+++ 02_Create_Stage_LoadData.sql
@@ -1,9 +1,9 @@
 /*
 STEP 2: Create a stage to store files.
-Download the books-dataset from Kaggle: https://www.kaggle.com/datasets/elvinrustam/books-dataset/data. 
-Upload BooksDatasetClean.csv to the stage.
-Load the staged file into a new table BOOKS_DATASET_RAW
-Note: First line of file contains the headers
+Next via Snowsight:
+   Upload BooksDatasetClean.csv to the stage.
+   Load the staged file into a new table BOOKS_DATASET_RAW
+      Note: First line of file contains the headers
 */
 
 USE DATABASE CORTEX_SEARCH_TUTORIAL_DB;
